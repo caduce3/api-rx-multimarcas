de1ea865c09eac9cc0dead38d10150bcba6cf44e
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-10-20 23:34:24 -0300

    feat: deletando arquivos que não serão utilizados

--- src/use-cases/@factories/clientes/make-add-player-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClientesRepository } from "@/repositories/prisma/prisma-clientes-repository";
-import { AddPlayerUseCase } from "@/use-cases/clientes/add-player";
-
-export function makeAddPlayerUseCase(){
-    const playersRepository = new PrismaClientesRepository()
-    const addPlayerUseCase = new AddPlayerUseCase(playersRepository)
-
-    return addPlayerUseCase
-}
